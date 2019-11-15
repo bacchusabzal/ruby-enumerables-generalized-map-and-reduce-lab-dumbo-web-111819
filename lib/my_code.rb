cfdb9fcacb9a381bb05d821950910ec0c0f5a2ee
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 09:46:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-# Your Code Here
-# def negative_arrays(array)
-  [1,2,3,-9].map { |n| n * -1}
-# end 
+# # Your Code Here
+# # def negative_arrays(array)
+#   [1,2,3,-9].map { |n| n * -1}
+# # end 
